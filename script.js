19780d811f859f4a598f3ee567a25bdf6a18c6ed
Jeremy <jrobertson.88@gmail.com>
2017-07-24 22:27:42 -0500

    Updated functionality with one final step to go

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-for (var i = 0; i <= 255; i++) {
-	$(".container").append('<div class="floating-box">text</div>');
-}
+//takes base input and creates base grid
+var boxSize = prompt("How many blocks in the box?", "50");
+var boxPrcnt = 100/boxSize;
+
+for (var i = 0; i <= boxSize; i++) {
+	$(".container").append('<div class="floating-box"></div>');
+};
+
+//turns blocks black
+$(".floating-box").mouseenter(function(){
+	$(this).addClass("floating-color").removeClass("floating-box");
+});	
+
+//clears box and takes new input
+function clrFunc(){
+	var boxSize = prompt("How many blocks in the box?", "50");
+	$(".floating-color").addClass("floating-box").removeClass("floating-color");
+	for (var i = 0; i <= boxSize; i++) {
+		$(".container").append('<div class="floating-box"></div>');
+	};
+};
 
